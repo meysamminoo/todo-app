596a681940c032778cc849218e50790325cf0e52
meysam <meisam.design.ph@gmail.com>
2022-02-10 14:54:55 +0330

    filtered todos

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
-const Navbar = ({ uncompletedTodo }) => {
+const Navbar = ({ uncompletedTodo, onSelect, status }) => {
+  if (!uncompletedTodo) return <h1>add some today todos</h1>;
+
   return (
     <header>
-      {uncompletedTodo ? (
-        <>
-          <span>{uncompletedTodo}</span>
-          <h1> not completed todos</h1>
-        </>
-      ) : (
-        <h1>add some today todos</h1>
-      )}
+      <span>{uncompletedTodo}</span>
+      <h1> not completed todos</h1>
+      <select onChange={onSelect} value={status}>
+        <option value="All">All</option>
+        <option value="Complated">Complated</option>
+        <option value="Uncomplated">Uncomplated</option>
+      </select>
     </header>
   );
 };
--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
 
 const TodoApp = () => {
   const [todos, setTodos] = useState([]);
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  const [status, setStatus] = useState("All");
+
+  useEffect(() => {
+    filteredTodosHandler(status);
+  }, [todos, status]);
 
   const addTodo = (input) => {
     const newTodo = {
@@ -44,12 +50,35 @@ const TodoApp = () => {
     setTodos(updatedTodos);
   };
 
+  const filteredTodosHandler = (status) => {
+    switch (status) {
+      case "Complated":
+        setFilteredTodos(todos.filter((t) => t.isCompleted));
+        break;
+      case "Uncomplated":
+        setFilteredTodos(todos.filter((t) => !t.isCompleted));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+
+  const selectHandler = (e) => {
+    setStatus(e.target.value);
+    filteredTodosHandler(e.target.value);
+  };
+
   return (
     <div className="container">
-      <Navbar uncompletedTodo={todos.filter((t) => !t.isCompleted).length} />
+      <Navbar
+        uncompletedTodo={todos.filter((t) => !t.isCompleted).length}
+        onSelect={selectHandler}
+        status={status}
+      />
       <TodoForm submitTodo={addTodo} />
       <TodoList
-        todos={todos}
+        todos={filteredTodos}
         onCompelet={CompeletHandler}
         onDelete={removeHandler}
         onUpdateTodo={updateTodo}
